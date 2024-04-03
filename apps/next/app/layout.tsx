4c3eaef495d1401694495eaf5bc5660acf81fbad
iamhectorsosa <hello@hectorsosa.me>
2024-04-03 10:22:20 +0200

    Updating layouts

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -67,47 +67,47 @@ export const metadata: Metadata = {
     startupImage: [
       "/apple-icon.png",
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-1242x2688.png",
         media:
           "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)",
       },
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-828x1792.png",
         media:
           "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)",
       },
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-1125x2436.png",
         media:
           "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
       },
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-1242x2208.png",
         media:
           "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)",
       },
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-750x1334.png",
         media:
           "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
       },
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-2048x2732.png",
         media:
           "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)",
       },
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-1668x2388.png",
         media:
           "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)",
       },
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-1668x2224.png",
         media:
           "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)",
       },
       {
-        url: "/apple-icon.png",
+        url: "/apple-launch-1536x2048.png",
         media:
           "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
       },
