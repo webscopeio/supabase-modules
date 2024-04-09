c312a5ad8ceac56c6905449581f78705617613b8
iamhectorsosa <hello@hectorsosa.me>
2024-04-09 07:06:37 +0200

    Updating server while researching middleware

--- apps/next/modules/utils/server.ts
+++ apps/next/modules/utils/server.ts
@@ -17,10 +17,18 @@ export function createClient(): ReturnType<
           return cookieStore.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options })
+          try {
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {
+            console.error(error)
+          }
         },
         remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: "", ...options })
+          try {
+            cookieStore.set({ name, value: "", ...options })
+          } catch (error) {
+            console.error(error)
+          }
         },
       },
     }
