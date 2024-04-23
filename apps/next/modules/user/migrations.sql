a78829b7724d61fc2414d754659631ac51d29e16
Rudolf Kotrla <71340231+rudokotrla@users.noreply.github.com>
2024-04-23 15:38:31 +0200

    fix: include migrations into squashed migrations file in the module folder

--- apps/next/modules/user/migrations.sql
+++ apps/next/modules/user/migrations.sql
@@ -41,6 +41,19 @@ AS $function$BEGIN
 END;$function$
 ;
 
+CREATE OR REPLACE FUNCTION public.update_user_email()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$BEGIN
+  UPDATE public.profiles 
+  SET email = NEW.email
+  WHERE id = NEW.id;
+
+  RETURN NEW;
+END;$function$
+;
+
 grant delete on table "public"."profiles" to "anon";
 
 grant insert on table "public"."profiles" to "anon";
@@ -117,3 +130,7 @@ with check ((auth.uid() = id));
 
 
 CREATE TRIGGER handle_new_user_trigger AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
+
+CREATE TRIGGER update_email_in_profiles_trigger AFTER UPDATE OF email ON auth.users FOR EACH ROW EXECUTE FUNCTION update_user_email();
+
+
