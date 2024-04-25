007312601e3c21a036cb045cec0ea4ebef55849a
iamhectorsosa <hello@hectorsosa.me>
2024-04-25 17:24:14 +0200

    fix:zod

--- apps/next/app/login/page.tsx
+++ apps/next/app/login/page.tsx
@@ -1,33 +1,12 @@
 import { redirect } from "next/navigation"
-import * as z from "zod"
+import { z } from "zod"
 
 import { LoginForm } from "@/components/user/login-form"
 
 import { createClient } from "@/modules/utils/server"
 
-const ErrorSchema = z
-  .string()
-  .transform((errorStr, ctx): z.infer<ReturnType<typeof JSON.parse>> => {
-    try {
-      return JSON.parse(errorStr)
-    } catch (e) {
-      ctx.addIssue({
-        message: "Invalid JSON string",
-        code: z.ZodIssueCode.custom,
-      })
-      return z.NEVER
-    }
-  })
-  .pipe(
-    z.object({
-      message: z.string(),
-      status: z.number(),
-    })
-  )
-  .catch({ message: "An error occurred", status: 500 })
-
 export default async function Page({
-  searchParams,
+  searchParams: { error },
 }: {
   searchParams: { error?: string }
 }) {
@@ -41,9 +20,22 @@ export default async function Page({
     redirect("/settings/accounts")
   }
 
-  const error = searchParams.error
-    ? ErrorSchema.parse(searchParams.error)
-    : undefined
+  if (error) {
+    return <LoginForm error={parseError(error)} />
+  }
 
-  return <LoginForm error={error} />
+  return <LoginForm />
+}
+
+const errorSchema = z.object({
+  message: z.string().catch("An error occurred"),
+  status: z.number().catch(500),
+})
+
+function parseError(error: string) {
+  try {
+    return errorSchema.parse(JSON.parse(error))
+  } catch (_error) {
+    return errorSchema.parse({})
+  }
 }
